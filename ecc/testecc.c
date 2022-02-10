59aa2439e64841284df3dadca50c67bb2d32fa41
langmar <langmar@uni-bremen.de>
2022-02-10 09:24:47 +0100

    added Gcrypt and wolfCrypt tests, both fail at verifying though.

--- ecc/testecc.c
+++ ecc/testecc.c
@@ -45,7 +45,14 @@
 #include "test_helper.h"
 #include <sodium.h>
 #include <gcrypt.h>
-//#include <wolfssl/ssl.h>
+#include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/settings.h>
+#include <wolfssl/ssl.h>
+#include <wolfssl/wolfcrypt/sha.h>
+#include <wolfssl/wolfcrypt/ecc.h>
+#include <wolfssl/wolfcrypt/curve25519.h>
+#include <wolfssl/wolfcrypt/random.h>
+#include <wolfssl/wolfcrypt/error-crypt.h>
 
 #ifdef CONTIKI
 #include "contiki.h"
@@ -216,11 +223,15 @@ void ecdsaSodiumTest() {
         unsigned long long signature_len;
 	ret = crypto_sign_detached(signature, &signature_len,
             message, message_len, sk);
-        assert(!ret);
+	if (ret != 0){
+	    puts("Libsodium signature verification failed");
+	}
 
         ret = crypto_sign_verify_detached(signature, message,
                                 message_len, pk);
-	assert(!ret);
+	if (ret != 0){
+	    puts("Libsodium signature verification failed");
+	}
 }
 
 
@@ -231,19 +242,41 @@ void ecdsaGcryptTest() {
             puts("Couldn't initialize libgcrypt");
         }
 	// Setup
-	gcry_sexp_t key_spec, key, pub_key, priv_key, data, signature;
+	gcry_sexp_t key_spec, key, pub_key, priv_key, signature; //data ;
+        gcry_mpi_t mpi_k = NULL;
+	gcry_md_hd_t hd;
         int rc;
-	rc = gcry_sexp_build(&data, NULL, "(data (flags pkcs1) (hash sha256 \"Hallo Welt!\"))");
+	size_t length = 32;
+	uint8_t digest[length];
+        //char *msg =  "Hallo Welt!";
+	const char *template = "(data (flags raw)(hash %s %b))";
+
+	
+ 
+	rc = gcry_md_open(&hd, GCRY_MD_SHA256, GCRY_MD_FLAG_SECURE);
+	if ((rc = gcry_mpi_scan (&mpi_k, GCRYMPI_FMT_USG, digest, length, NULL))){ 
+            printf("error converting MPI: %s\n", gcry_strerror(rc));
+    	}
+
+	gcry_md_write(hd, ecdsaTestMessage, sizeof(ecdsaTestMessage)/sizeof(ecdsaTestMessage[0]));
+	/*	
+        rc = gcry_sexp_build(&data, NULL, "(data (flags raw) (value %m))", mpi_k);
         if (rc) {
             printf("converting data for encryption failed: %s\n", gcry_strerror(rc));
         }
+*/
         // Keygen
 
-        rc = gcry_sexp_new(&key_spec, "(genkey (ecdsa (curve \"Curve25519\")))", 0, 1);
+        rc = gcry_sexp_new(&key_spec, "(genkey (ecc (curve \"Curve25519\")))", 0, 1);
         rc = gcry_pk_genkey(&key, key_spec);      
         if (rc) {
             printf("error creating S-expression: %s\n", gcry_strerror(rc));
         }
+	rc = gcry_pk_testkey(key);
+        if (rc) {
+            printf("error testing key: %s\n", gcry_strerror(rc));
+        }
+	gcry_sexp_release(key_spec);
         pub_key = gcry_sexp_find_token(key, "public-key", 0);
         if (!pub_key) {
             printf("public part missing in key\n");
@@ -252,18 +285,69 @@ void ecdsaGcryptTest() {
         if (!priv_key) {
             printf("privatepart missing in key\n");
         }
- 
-        // Create Signature
-	rc = gcry_pk_sign(&signature, data, priv_key);
+	gcry_sexp_release(key);
+	gcry_sexp_dump(pub_key);
+	gcry_sexp_dump(priv_key);
+	//gcry_sexp_dump(data) 
+        
+	// Create Signature
+	rc = gcry_pk_hash_sign(&signature, template, priv_key, hd, NULL);
         if (rc) {
             printf("error signing data: %s\n", gcry_strerror(rc));
         }
-
-	rc = gcry_pk_verify(signature, data, pub_key);
+	gcry_sexp_release(priv_key);
+	gcry_sexp_dump(signature);
+	gcry_md_final(hd);
+	printf("Hash: %s \n", gcry_md_read(hd, GCRY_MD_SHA256));
+	rc = gcry_pk_hash_verify(signature, template, pub_key, hd, NULL);
         if (rc) {
-            printf("error verifying data: %s\n", gcry_strerror(rc));
+            printf("error verifying data: %i %s\n", rc-GPG_ERR_INTERNAL, gcry_strerror(rc));
         }
+	gcry_sexp_release(signature);
+	gcry_sexp_release(pub_key);
+	gcry_md_close(hd);
 }
+
+void ecdsaWolfcryptTest(){
+	int ret, verified = 0;
+	unsigned int sigSz;
+	ret = wolfSSL_Init();
+	if (ret != SSL_SUCCESS) {
+	    puts("failed to initialize wolfSSL");
+	}
+
+	ecc_key key;
+	RNG rng;
+	Sha sha;
+	byte sig[512];
+	byte digest[SHA_DIGEST_SIZE];
+	sigSz = sizeof(sig);
+
+	//Hash
+	wc_InitSha(&sha);
+	wc_ShaUpdate(&sha, (byte*)ecdsaTestMessage, sizeof(ecdsaTestMessage)/sizeof(ecdsaTestMessage[0]));
+	wc_ShaFinal(&sha, digest);
+	//RNG
+	wc_InitRng(&rng);
+	//Key Gen
+	wc_ecc_init(&key);
+	wc_ecc_make_key(&rng, 32, &key);
+
+	ret = wc_ecc_sign_hash(digest, sizeof(digest), sig, &sigSz, &rng, &key);
+	if (ret != 0){
+	    puts("Error wolfcrypt generating signature");
+	}
+
+	ret = wc_ecc_verify_hash(sig,sizeof(sig), digest, sizeof(digest), &verified, &key);
+	if (ret != 0){
+	    printf("Error %i while wolfcrypt verifying signature\n", ret);
+	}
+	if (ret == BAD_FUNC_ARG){
+	    puts("passed NULL somehow");
+	}
+}
+
+
 #ifdef CONTIKI
 PROCESS(ecc_test, "ECC test");
 AUTOSTART_PROCESSES(&ecc_test);
@@ -292,6 +376,7 @@ int main(int argc, char const *argv[])
 	//ecdsaTest();
 	ecdsaSodiumTest();
 	ecdsaGcryptTest();
+	ecdsaWolfcryptTest();
 	printf("%s\n", "All Tests successful.");
 	return 0;
 }
